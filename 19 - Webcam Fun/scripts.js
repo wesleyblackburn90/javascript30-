cb0b6d1e53ad51a5e8c50fbcbdd5911b0135965b
Wesley Blackburn <wesley.blackburn90@yahoo.com>
2024-05-24 21:22:51 -0400

    JS30 day 19 pt 2

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -18,14 +18,44 @@ function getVideo() {
 }
 
 function paintToCanvas() {
-  const width = video.videoWidth
-  const height = video.videoHeight
-  canvas.width = width
-  canvas.height = height
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
 
   return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height)
-  }, 16)
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+
+    // pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
 }
 
+function takePhoto() {
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+
+
 getVideo()
+
+
+video.addEventListener('canplay', paintToCanvas)
